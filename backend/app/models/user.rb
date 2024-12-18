d26f359b7e9318091ee6e2c60d534679e5b901fc
AMT98 <aswinmalla12@gmail.com>
2024-12-18 10:51:55 -0700

    Signup route created

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI: :MailTo: :EMAIL_REGEXP, message: "Email address must be valid" }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Invalid email format" }
 end
