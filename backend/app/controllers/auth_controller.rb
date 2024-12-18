d7d85759526d4e8fd30584507a4d52d9388dda26
AMT98 <aswinmalla12@gmail.com>
2024-12-18 11:05:43 -0700

    Created login routes and added login to auth controller to generate jwt token as well

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -9,9 +9,25 @@ class AuthController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      token = encode_token({ user_id: user.id })
+      render json: {token: token, user: user}, status: :ok
+    else 
+      render json: { error: "Invalid email or passowrd" }, status: unauthrorized
+    end
+  end
+
   private
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)
+  end
+
 end
