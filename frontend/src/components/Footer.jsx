545fe3881228047b181c388edb34a3e6ecc41f0e
AMT98 <aswinmalla12@gmail.com>
2024-12-22 13:45:01 -0700

    Created footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-white w-full fixed bottom-0 left-0">
+      <div className="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8">
+        <div className="mt-16 border-t border-gray-100 pt-8">
+          <p className="text-center text-xs/relaxed text-gray-500">
+            © Splitwise Practice 2024. All rights reserved.
+            <br />
+            Created with{" "}
+            <span className="font-bold">Ruby on Rails and React</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
