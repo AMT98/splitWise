e6ff0b21e89df1de1dca7c633473b2cf02cd5b33
AMT98 <aswinmalla12@gmail.com>
2024-12-22 13:27:44 -0700

    Created login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full bg-white fixed top-0 left-0 z-50">
+      <nav className="container mx-auto p-4 flex justify-between items-center min-w-full">
+        <div>
+          <h1 className="text-3xl self-center">Splitwise</h1>
+        </div>
+        <div className="flex gap-4">
+          <Link to="/login">
+            <button className="border p-4 bg-indigo-500 text-white font-bold rounded-md">
+              Login
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="border p-4 bg-gray-500 text-white font-bold rounded-md">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
