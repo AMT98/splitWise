4f0c050c7e7c83c198e33b94310fdd226ecd68c6
AMT98 <aswinmalla12@gmail.com>
2024-12-22 20:06:22 -0700

    Fixed cors issue and made changes to auth controller

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -12,21 +12,27 @@ class AuthController < ApplicationController
   end
 
   def login
-    user = User.find_by(email: params[:email])
-
-    if user&.authenticate(params[:password])
+    email = params.dig(:auth, :email) || params[:email]
+    password = params.dig(:auth, :password) || params[:password]
+  
+    user = User.find_by(email: email)
+  
+    if user&.authenticate(password)
       token = encode_token({ user_id: user.id })
-      render json: {token: token, user: user}, status: :ok
-    else 
-      render json: { error: "Invalid email or passowrd" }, status: unauthrorized
+      puts "Generated Token: #{token}"
+      render json: { token: token, user: user }, status: :ok
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
     end
   end
+  
 
   private
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    params.require(:auth).permit(:email, :password, :password_confirmation)
   end
+  
 
   def encode_token(payload)
     JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
