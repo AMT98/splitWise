e626dd2b1be556b214723e1512074ebfbd6952f3
AMT98 <aswinmalla12@gmail.com>
2025-01-02 13:32:33 -0700

    Removed fixed class for footer so it sits at the bottom of the page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-white w-full fixed bottom-0 left-0 mt-12">
+    <footer className="bg-white w-full mt-12">
       <div className="mx-auto max-w-screen-xl px-4 pb-8 sm:px-6 lg:px-8">
-        <div className=" border-t border-gray-100 pt-8">
+        <div className="border-t border-gray-100 pt-8">
           <p className="text-center text-xs/relaxed text-gray-500">
             © Splitwise Practice 2024. All rights reserved.
             <br />
